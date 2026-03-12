47cc07ca509da7e8eefd7aacc73bf49ad5371bb3
neylorfm <neylor.prof@gmail.com>
2026-03-12 20:37:52 -0300

    fix: corrige importação das fontes para build do cloudflare

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import '@fontsource/geist-sans'
-import '@fontsource/geist-mono'
+import '@fontsource/geist-sans/index.css'
+import '@fontsource/geist-mono/index.css'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
